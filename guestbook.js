55d7824d308445d409b0336c5c68eb86ced5f141
Thales Henrique Gomes Lobato <51369355+Thales36@users.noreply.github.com>
2022-05-20 11:11:49 -0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f6d08924.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
